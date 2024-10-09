bbbb5d305724f4d58f3eee9ef26a7243ed53da4b
kateroook <severinakat11@gmail.com>
2024-10-09 21:36:52 +0200

    Add Cart Entities

---
+++ CustmeWebApp/Models/CartItem.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustmeWebApp.Models
+{
+    public class CartItem
+    {
+        public int Id { get; set; }
+        [Required]
+        public int CartId { get; set; }
+        [Required]
+        public int ServiceId { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        public Service Service { get; set; }
+        public Cart Cart { get; set; }
+    }
+}
