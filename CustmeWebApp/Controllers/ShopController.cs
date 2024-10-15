d57db05068bdb662538c084cd59da80ebcf8f70a
kateroook <severinakat11@gmail.com>
2024-10-15 19:55:06 +0200

    Shop Index page

---
+++ CustmeWebApp/Controllers/ShopController.cs
@@ -0,0 +1,41 @@
+﻿using CustmeWebApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustmeWebApp.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ShopController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Projects
+                .Include(p => p.Service);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var project = await _context.Projects
+                .Include(p => p.Service)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return View(project);
+        }
+
+    }
+}
