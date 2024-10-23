f9514708282e55a05c8b2eb2f75314e01d95e83b
kateroook <severinakat11@gmail.com>
2024-10-23 21:43:10 +0200

    Revert "google authentication added"
    This reverts commit 6fc3faa9cfc5d892559abaa9a98ec1892ac0648e.

--- CustmeWebApp/Program.cs
+++ CustmeWebApp/Program.cs
@@ -2,7 +2,6 @@ using CustmeWebApp.Data;
 using CustmeWebApp.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,15 +25,6 @@ builder.Services
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultUI()
     .AddDefaultTokenProviders();
-
-builder.Services.AddAuthentication()
-        .AddGoogle(options =>
-        {
-            IConfigurationSection googleAuthSection =
-                builder.Configuration.GetSection("Authentication:Google");
-            options.ClientId = googleAuthSection["ClientId"];
-            options.ClientSecret = googleAuthSection["ClientSecret"];
-        });
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddControllers().AddJsonOptions(options =>
