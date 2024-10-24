6bdfff6d9e81968f95cb87aba3f50535cd86af6a
kateroook <severinakat11@gmail.com>
2024-10-24 08:34:53 +0200

    Revert "google authentication added"
    This reverts commit a7ba016dc2a800de74d9ce46823a5a799257108b.

--- CustmeWebApp/Program.cs
+++ CustmeWebApp/Program.cs
@@ -1,10 +1,7 @@
 using CustmeWebApp.Data;
 using CustmeWebApp.Models;
-using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.Google;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,15 +25,6 @@ builder.Services
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultUI()
     .AddDefaultTokenProviders();
-
-builder.Services.AddAuthentication()
-.AddCookie()
-.AddGoogle(googleOptions =>
-{
-    googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
-    googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
-    //googleOptions.CallbackPath = "/signin-google"; 
-});
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddControllers().AddJsonOptions(options =>
@@ -101,13 +89,11 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-app.UseAuthentication();
 
 app.UseSession();
 
 app.UseCors("AllowAll");
 
-app.MapControllers();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
